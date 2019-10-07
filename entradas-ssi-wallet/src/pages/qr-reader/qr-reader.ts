7fe7c1b0bcf2b632ef79eabe18725c3947e88091
Nil G <nil.guerrero@in2.es>
2019-10-07 18:42:58 +0200

    CHANGE: Decoding method of the JWT from de CredentialPresentation now works!

--- entradas-ssi-wallet/src/pages/qr-reader/qr-reader.ts
+++ entradas-ssi-wallet/src/pages/qr-reader/qr-reader.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {QRScanner, QRScannerStatus} from "@ionic-native/qr-scanner/ngx";
 import {BarcodeScanner} from "@ionic-native/barcode-scanner";
 import {Login} from "../login/login";
@@ -17,69 +17,67 @@ import {Base64} from "js-base64";
 
 @IonicPage()
 @Component({
-  selector: 'page-qr-reader',
-  templateUrl: 'qr-reader.html',
+    selector: 'page-qr-reader',
+    templateUrl: 'qr-reader.html',
     styleUrls: ['/qr-reader.scss']
 })
 export class QrReaderPage {
     user: User = null;
-    elementType : 'url' | 'canvas' | 'img' = 'url';
+    elementType: 'url' | 'canvas' | 'img' = 'url';
 
-  constructor(public navCtrl: NavController,
-              public navParams: NavParams,
-              private barcode: BarcodeScanner,
-              private testService: TestService) {
-  }
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                private barcode: BarcodeScanner,
+                private testService: TestService) {
+    }
 
-  ionViewDidLoad() {
-      console.log('ionViewDidLoad QrReaderPage');
-  }
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad QrReaderPage');
+    }
 
-  // qrScannerCam() {
-  //     console.log("ENTRA");
-  //     this.barcode.scan().then(barcode =>{
-  //         console.log("Ok escanneando",barcode);
-  //         this.user = JSON.parse(barcode.text);
-  //         this.navCtrl.push(QrResponsePage,{user:this.user})
-  //     }).catch(err =>{
-  //         console.log("Nope");
-  //     })
-  // }
+    // qrScannerCam() {
+    //     console.log("ENTRA");
+    //     this.barcode.scan().then(barcode =>{
+    //         console.log("Ok escanneando",barcode);
+    //         this.user = JSON.parse(barcode.text);
+    //         this.navCtrl.push(QrResponsePage,{user:this.user})
+    //     }).catch(err =>{
+    //         console.log("Nope");
+    //     })
+    // }
 
     qrScannerCam() {
         this.barcode.scan().then(barcodeData => {
             if (!barcodeData) {
                 alert('Error: Contacte con el service provider.')
             } else {
-                console.log('ENTRA')
-                let dataQR = JSON.parse(barcodeData.text);
-                this.searchJSON(dataQR);
+                let jwt = require("jsonwebtoken");
+                let token = jwt.decode(barcodeData.text);
+                this.searchJSON(token);
             }
         }).catch(err => {
             console.log('Error', err);
         });
     }
 
-    searchJSON(data: any){
-        for(let k in data){
-            if(typeof  data[k]=="object" && data[k]!==null){
-                this.searchJSON(data[k]);
-            }
-            else{
-                if(k=='verifiableCredential'){
-                    console.log(data[k]);
+    searchJSON(data: any) {
+        for (let k in data) {
+            if (typeof data[k] == "object" && data[k] !== null) {
+                if (k == 'verifiableCredential') {
                     let decode64 = Base64.decode(data[k]);
-                    console.log(decode64);
-                    this.navCtrl.push(QrResponsePage,{wantedRq:decode64});
-                }else{
-                    alert("RECHZADO");
+                    this.navCtrl.push(QrResponsePage, {wantedRq: decode64});
                 }
+                else{
+                    this.searchJSON(data[k]);
+                }
+            } else {
+
             }
         }
     }
 
-  goBack(){
-      this.testService.getAlastriaID();
-      this.navCtrl.pop();
-  }
+    goBack() {
+        this.testService.getAlastriaID();
+        this.navCtrl.pop();
+    }
 }
